0a09a95f236cfdefe9db92f1668ee9252fd8922c
cleargoaldigital <cleargoal.ng@gmail.com>
2022-01-07 23:33:00 +0100

    updated codes

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
 // setting constants and variables
 
-const container = document.querySelector('.container');
-let rows = document.getElementsByClassName('gridRow');
-let celss = document.getElementsByClassName('cell');
+const container = document.getElementById("container");
 
-// creating a grid function of 16 x 16
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
 
-function sketchGrid(){
-    makeRows(16);
-    makeColumns(16);
+makeRows(16, 16);
+
+
+/**
+ * let n=16, i=-1, j=0, s='';
+
+while(++i<n) {
+  s+= '<div class="row">'
+  for(j=0; j<n; j++) s+= `<div class="cell"> ${(i*n+j).toString(16)} </div>`;
+  s+= '</div>'
 }
 
-// making a grid with rows and columns
+container.innerHTML = s;
+.row { display: flex; font-size: 9.5px; text-align: center; color: red }
+.cell { width: 10px; height: 10px; margin: 1px; border: 1px solid black;}
+<div id="container"></div>
+ */
 
-function makeRows(rowNum){
-    for(r = 0; r <= rowNum; r++){
-        let row = document.createElement('div');
-        container.appendChild(row).className = "gridRow";
-    }
+/**
+ * const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
+ */
+
+/**
+ 
+let n=16, i=-1, j=0, s='';
+
+while(++i<n) {
+  s+= '<div class="row">'
+  for(j=0; j<n; j++) s+= `<div class="cell"> ${(i*n+j).toString(16)} </div>`;
+  s+= '</div>'
 }
 
-function makeColumns(cellNum){
-    for (i = 0; i < rows.length; i++){
-        for(j = 0; j < cellNum; j++){
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = "cell"
-        }
-    }
-}
+container.innerHTML = s;
+ */
