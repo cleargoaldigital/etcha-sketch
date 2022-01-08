fb9e324f73292aaf2665ebd9eba254d025e973fc
cleargoaldigital <cleargoal.ng@gmail.com>
2022-01-08 22:57:51 +0100

    updated files

--- main.js
+++ main.js
@@ -1,60 +1,31 @@
-// setting constants and variables
+//  setting constants and initializing values
 
 const container = document.getElementById("container");
+const question = document.querySelector('.question')
+
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
+  for (i = 1; i <= (rows * cols); i++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
+    cell.innerText = (i);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 makeRows(16, 16);
 
+container.addEventListener('click', () => {
+  container.style.backgroundColor = 'orange';
+});
 
-/**
- * let n=16, i=-1, j=0, s='';
+question.addEventListener('click', () => {
+  // container.textContent = " ";
+  let userReply = Number(prompt('How many grids are there in a row?'))
 
-while(++i<n) {
-  s+= '<div class="row">'
-  for(j=0; j<n; j++) s+= `<div class="cell"> ${(i*n+j).toString(16)} </div>`;
-  s+= '</div>'
-}
-
-container.innerHTML = s;
-.row { display: flex; font-size: 9.5px; text-align: center; color: red }
-.cell { width: 10px; height: 10px; margin: 1px; border: 1px solid black;}
-<div id="container"></div>
- */
-
-/**
- * const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-makeRows(16, 16);
- */
-
-/**
- 
-let n=16, i=-1, j=0, s='';
-
-while(++i<n) {
-  s+= '<div class="row">'
-  for(j=0; j<n; j++) s+= `<div class="cell"> ${(i*n+j).toString(16)} </div>`;
-  s+= '</div>'
-}
-
-container.innerHTML = s;
- */
+  // let cell = document.createElement("div");
+  // cell.textContent = (i);
+  // container.appendChild(cell).className = "grid-item";
+ // { for (i = 1; i <= userReply; i++)}
+});
